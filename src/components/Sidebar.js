864b26447b0cd3ab26e5970e50057576b2e5e11e
蔡承佑 <tim.tsai@kdanmobile.com>
2024-08-13 02:43:40 +0800

    Add update event

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { updateItem } from '../actions';
 import Item from './SidebarItem';
 import './Sidebar.css';
 
-const Sidebar = ({ selectedItem }) => {
+const Sidebar = ({ selectedItem, updateItem }) => {
   return (
     <div>
       { selectedItem ? (
         <div className='container'>
-          <h1>{selectedItem.name}</h1>
-          <p>{selectedItem.type}</p>
+          {
+            selectedItem.type === 'image' ? (
+              <div>
+                <input type="text" defaultValue={selectedItem.height} onChange={(e) => {
+                  if (e.target.value.endsWith('px')) {
+                    updateItem(selectedItem.id, { height: e.target.value })
+                  }
+                }} />
+                <input type="text" defaultValue={selectedItem.width} onChange={(e) => { 
+                  if (e.target.value.endsWith('px')) {
+                    updateItem(selectedItem.id, { width: e.target.value })
+                  }
+                }} />
+                <input type="text" defaultValue={selectedItem.src} onChange={(e) => {
+                  // check if the value is a valid URL
+                  if (e.target.value.match(/(http(s?):)?([/|.|\w|\s|-])*\.(?:jpg|gif|png)/g)) {
+                    updateItem(selectedItem.id, { src: e.target.value })
+                  }
+                }} />
+              </div>
+            ) : (
+              <input type="text" value={selectedItem.value} onChange={(e) => updateItem(selectedItem.id, { value: e.target.value })} />
+            )
+          }
         </div>
       ) : (
         <div className='container'>
@@ -25,4 +48,8 @@ const mapStateToProps = (state) => ({
   selectedItem: state.selectedItem
 });
 
-export default connect(mapStateToProps)(Sidebar);
+const mapDispatchToProps = (dispatch) => ({
+  updateItem: (itemId, newProps) => dispatch(updateItem(itemId, newProps))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Sidebar);
