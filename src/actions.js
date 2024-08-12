af10904842ee5d96f45d47006d5912d4cbcb5060
蔡承佑 <tim.tsai@kdanmobile.com>
2024-08-13 02:43:51 +0800

    Fix default value

--- src/actions.js
+++ src/actions.js
@@ -2,7 +2,7 @@
 export const addItem = (item) => {
   let payload = { ...item, id: Date.now() };
   if (item.type === 'image') {
-    payload = { ...payload, src: 'logo192.png', width: 100, height: 100, value: 'This is an Image' };
+    payload = { ...payload, src: 'logo192.png', width: '100px', height: '100px', value: 'This is an Image' };
   } else {
     payload = { ...payload, value: 'This is a Text' };
   }
